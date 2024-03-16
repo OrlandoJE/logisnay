11d61f09c0a2c247793b540ff19f8dfdc4cfece2
Orlando <131704670+OrlandoJE@users.noreply.github.com>
2024-03-16 17:18:02 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,83 @@
+// Para la animacion de la barra de navegacion
+// Obtén la barra de navegación
+const navbar = document.querySelector('.navbar');
+
+// Agrega un evento de desplazamiento para controlar el cambio de tamaño
+window.addEventListener('scroll', () => {
+    // Verifica la posición de desplazamiento
+    if (window.scrollY > 100) { // Cambia 100 a la cantidad de desplazamiento en píxeles en la que deseas que ocurra el cambio
+        navbar.classList.add('scroll-shrink'); // Agrega una clase para reducir el tamaño
+    } else {
+        navbar.classList.remove('scroll-shrink'); // Remueve la clase si el desplazamiento es menor
+    }
+});
+
+const navbarhr = document.querySelector('.hr');
+
+// Agrega un evento de desplazamiento para controlar el cambio de tamaño
+window.addEventListener('scroll', () => {
+    // Verifica la posición de desplazamiento
+    if (window.scrollY > 100) { // Cambia 100 a la cantidad de desplazamiento en píxeles en la que deseas que ocurra el cambio
+        navbarhr.classList.add('scroll-shrink'); // Agrega una clase para reducir el tamaño
+    } else {
+        navbarhr.classList.remove('scroll-shrink'); // Remueve la clase si el desplazamiento es menor
+    }
+});
+
+// Preguntas frecuentes //
+document.addEventListener("DOMContentLoaded", function () {
+    const preguntas = document.querySelectorAll(".pregunta");
+
+    preguntas.forEach((pregunta) => {
+        pregunta.addEventListener("click", function () {
+            const parentLi = pregunta.parentElement;
+            const respuesta = parentLi.querySelector(".respuesta");
+
+            parentLi.classList.toggle("active");
+
+            if (parentLi.classList.contains("active")) {
+                respuesta.style.maxHeight = respuesta.scrollHeight + "px";
+            } else {
+                respuesta.style.maxHeight = "0";
+            }
+        });
+    });
+});
+
+// Animacion scroll //
+// Función para manejar la animación de fade-in en todos los elementos con la clase "box"
+function handleIntersection(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            // Cuando el elemento es visible en el viewport
+            entry.target.style.opacity = "1";
+        }
+    });
+}
+
+// Configuración del Intersection Observer
+const options = {
+    root: null, // Use el viewport como el elemento de referencia
+    rootMargin: "0px", // Sin margen adicional
+    threshold: 0.1 // Reduce el valor a 0.1 (10% del elemento visible)
+};
+
+
+// Crear una instancia del Intersection Observer
+const observer = new IntersectionObserver(handleIntersection, options);
+
+// Observar todos los elementos
+const boxes = document.querySelectorAll("*");
+boxes.forEach(box => {
+    observer.observe(box);
+});
+
+// Menu desplegable //
+document.addEventListener("DOMContentLoaded", function () {
+    const menuButton = document.querySelector(".barras-menu");
+    const menu = document.querySelector(".menu-desplegable");
+
+    menuButton.addEventListener("click", function () {
+        menu.classList.toggle("menu-open");
+    });
+});
