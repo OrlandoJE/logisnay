9c49bc88255b47db72198a9616d5dfe6a2f30cdb
Orlando <131704670+OrlandoJE@users.noreply.github.com>
2024-03-27 11:35:21 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,60 @@
+// Animacion scroll //
+window.onload = function () {
+
+    // Función para manejar la animación de fade-in en todos los elementos
+    function handleIntersection(entries, observer) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                // Cuando el elemento es visible en el viewport
+                entry.target.style.opacity = "1";
+            }
+        });
+    }
+
+    // Configuración del Intersection Observer
+    const options = {
+        root: null, // Use el viewport como el elemento de referencia
+        rootMargin: "0px", // Sin margen adicional
+        threshold: 0.1 // Reduce el valor a 0.1 (10% del elemento visible)
+    };
+
+    // Crear una instancia del Intersection Observer
+    const observer = new IntersectionObserver(handleIntersection, options);
+
+    // Observar todos los elementos
+    const boxes = document.querySelectorAll("*");
+    boxes.forEach(box => {
+        observer.observe(box);
+    });
+
+}
+
+// Menu desplegable //
+document.addEventListener("DOMContentLoaded", function () {
+    const menuButton = document.querySelector(".barrasMenu");
+    const menu = document.querySelector(".menuDesplegable");
+
+    menuButton.addEventListener("click", function () {
+        this.classList.toggle("active");
+        menu.classList.toggle("menuOpen");
+    });
+});
+
+document.getElementById('formulario').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    var nombre = document.getElementById('nombre').value;
+    var apellido = document.getElementById('apellido').value;
+    var tel = document.getElementById('tel').value;
+    var email = document.getElementById('email').value;
+    var asunto = document.getElementById('asunto').value;
+
+    var mailtoLink = 'mailto:fabian.hernandez@logisnay.com?subject=' + encodeURIComponent('Página Web Logisnay') +
+        '&body=' + encodeURIComponent(nombre + ' ' + apellido + '\nTeléfono: ' + tel + '\nCorreo Electrónico: ' + email + '\n\n Asunto del Cliente: \n' + asunto);
+
+    window.location.href = mailtoLink;
+});
+
 // Preguntas frecuentes //
 document.addEventListener("DOMContentLoaded", function () {
     const preguntas = document.querySelectorAll(".pregunta");
@@ -16,58 +73,4 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         });
     });
-});
-
-// Animacion scroll //
-// Función para manejar la animación de fade-in en todos los elementos con la clase "box"
-function handleIntersection(entries, observer) {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            // Cuando el elemento es visible en el viewport
-            entry.target.style.opacity = "1";
-        }
-    });
-}
-
-// Configuración del Intersection Observer
-const options = {
-    root: null, // Use el viewport como el elemento de referencia
-    rootMargin: "0px", // Sin margen adicional
-    threshold: 0.1 // Reduce el valor a 0.1 (10% del elemento visible)
-};
-
-
-// Crear una instancia del Intersection Observer
-const observer = new IntersectionObserver(handleIntersection, options);
-
-// Observar todos los elementos
-const boxes = document.querySelectorAll("*");
-boxes.forEach(box => {
-    observer.observe(box);
-});
-
-// Menu desplegable //
-document.addEventListener("DOMContentLoaded", function () {
-    const menuButton = document.querySelector(".barrasMenu");
-    const menu = document.querySelector(".menuDesplegable");
-
-    menuButton.addEventListener("click", function () {
-        this.classList.toggle("active");
-        menu.classList.toggle("menuOpen");
-    });
-});
-
-document.getElementById('formulario').addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    var nombre = document.getElementById('nombre').value;
-    var apellido = document.getElementById('apellido').value;
-    var tel = document.getElementById('tel').value;
-    var email = document.getElementById('email').value;
-    var asunto = document.getElementById('asunto').value;
-
-    var mailtoLink = 'mailto:fabian.hernandez@logisnay.com?subject=' + encodeURIComponent('Página Web Logisnay') +
-        '&body=' + encodeURIComponent(nombre + ' ' + apellido + '\nTeléfono: ' + tel + '\nCorreo Electrónico: ' + email + '\n\n Asunto del Cliente: \n' + asunto);
-
-    window.location.href = mailtoLink;
 });
